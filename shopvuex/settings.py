02586aaef39c90d7f7c662795a33e04bf73f7e97
Alisher007 <alisherx07@gmail.com>
2020-01-04 18:29:45 +0400

    static files added

--- shopvuex/settings.py
+++ shopvuex/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'shopvuex.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = ( os.path.join('static'), )
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
